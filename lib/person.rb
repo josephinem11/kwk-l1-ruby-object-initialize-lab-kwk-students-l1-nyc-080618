6acbb66cc43cdfa76e7dcf195133d61cf09774ff
josephinem11 <josephine.man0112@yahoo.com>
2018-08-10 16:56:36 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,7 @@
-class Person 
-  def initialize (name)
-    @name = name 
-  end
-#   def name
-#     @name = 
-#   end
-#   person_one = Person.new('Mia')
-#   puts person_one.name
-# end
+class Person
+def initialize (new_name)
+  @name=new_name
+end
+
+person_one=person_one.new('Mia')
+end
